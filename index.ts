bf30d9e4e4b6909d734b7142c353c39ae49f46c1
David Vílchez <dvilchez@xuaps.com>
2021-04-25 11:43:26 +0200

    Add public endpoints

---
+++ index.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response, NextFunction } from "express";
+import routes from "./src/infrastructure/routes";
+import cors from "cors";
+
+const app: express.Application = express();
+const port = process.env.PORT || 3000;
+
+app.disable("x-powered-by");
+
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(express.json());
+app.use("/", routes);
+app.use(
+  (error: Error, req: Request, res: Response, next: NextFunction): Response => {
+    return res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ src/infrastructure/routes.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import multer from "multer";
+import config from "config";
+import { GetDocument, AddDocument } from "../file-management/usecases";
+import { BlobStorage, createBlobServiceClient } from "./blob-storage-repo";
+
+const connection = createBlobServiceClient(
+  config.get("Azure.Storage.AccountName"),
+  config.get("Azure.Storage.AccountKey")
+);
+const blobStorage = new BlobStorage(
+  connection,
+  config.get("Documents.ContainerName")
+);
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+const router = Router();
+
+router.get("/documents/:slug", async (req, res) => {
+  const doc = await new GetDocument(blobStorage).execute(req.params.slug);
+
+  res.send(doc);
+});
+
+router.post("/documents/", upload.single("document"), async (req, res) => {
+  const id = await new AddDocument(blobStorage, 64).execute(req.file.buffer);
+
+  res.send({ id });
+});
+
+export default router;
