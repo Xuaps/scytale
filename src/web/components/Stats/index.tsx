98663061c6d99476e3c284db47a02e333014f708
David Vílchez <dvilchez@xuaps.com>
2021-11-09 20:16:06 +0100

    Add table to stats

--- src/web/components/Stats/index.tsx
+++ src/web/components/Stats/index.tsx
@@ -2,36 +2,45 @@ import React, { useEffect } from "react";
 import { SharedFile } from "../../model";
 import { FileStatsState } from "../../store";
 
-const Stats = ({
-  id,
-  state,
-  onLoad,
-}: {
-  id: string
-  state: FileStatsState
-  onLoad: (file: SharedFile) => void
-}) => {
+const Stats = ({ id, state, onLoad }: { id: string; state: FileStatsState; onLoad: (file: SharedFile) => void }) => {
   useEffect(() => {
-    onLoad({id, password: ""})
+    onLoad({ id, password: "" });
   }, []);
 
   if (!state.loaded) return <div>loading stats...</div>;
 
-  return (<div>
-    {state.stats.map(f => `
-      ${f.type}&nbsp;
-      ${f.model}&nbsp;
-      ${f.os}&nbsp;
-      ${f.ip}&nbsp;
-      ${f.city}&nbsp;
-      ${f.stateOrProvince}&nbsp;
-      ${f.countryOrRegion}&nbsp;
-      ${f.browser}&nbsp;
-      ${f.timestamp}&nbsp;
-    `)}
-  </div>
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Model</th>
+          <th>OS</th>
+          <th>IP</th>
+          <th>City</th>
+          <th>State or province</th>
+          <th>Country or region</th>
+          <th>Browser</th>
+          <th>Timestamp</th>
+        </tr>
+      </thead>
+      <tbody>
+        {state.stats.map((r) => (
+          <tr>
+            <td>{r.type}</td>
+            <td>{r.model}</td>
+            <td>{r.os}</td>
+            <td>{r.ip}</td>
+            <td>{r.city}</td>
+            <td>{r.stateOrProvince}</td>
+            <td>{r.countryOrRegion}</td>
+            <td>{r.browser}</td>
+            <td>{r.timestamp}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
 export default Stats;
-
