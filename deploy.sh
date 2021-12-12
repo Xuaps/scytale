45f0b0603ab7d3354d4ff3e9af6666bbb51573e7
David Vílchez <dvilchez@xuaps.com>
2021-12-12 18:24:19 +0100

    Add readme file

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+//Provision using terraform configuration
+//Get variables values
+//Add API permissions to access to the Container
+
+//PENDING -- code has instrumentationkey and serverurl hard coded
+
+//copy static files to container
+az storage blob upload-batch —account-name xuapsapimsadevelopmentwe -d ‘$web’ -s dist/.
+
+//PENDING -- Logger isn’t working
+
+//Different environmnets
+Develop environment 
+Prod??? Preserve terraform fies on githu actions
+
+//Stats endpoint policy requires
+app id
+api key
+——
+
---
+++ readme.md
@@ -0,0 +1,19 @@
+This is mainly a pet project to test new things I want to learn.
+
+Pending tasks
+
+* Finish deployment script
+* Configuration by environment
+* Error boundary
+* Log, traces and metrics
+* Rate limiters by file
+
+Next features
+
+* Validate uploaded files with a lambda
+* yubikey auth
+* Watermark
+* Remove metada
+* Steganography
+* Upload folders
+
