bd985422c75180961536c017c87f11e9f7dd54f8
David Vílchez <dvilchez@xuaps.com>
2022-06-27 17:20:18 +0200

    Add husky

--- src/user_stories/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as useGetStats } from "./get_stats";
-export { default as useDeleteFile } from "./delete_file";
-export { default as useDownloadFile } from "./download_file";
-export { default as useUploadFile } from "./upload_file";
