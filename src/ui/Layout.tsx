276e712951090eed61bcf3c7d8c90f8ac1b0c7c2
David Vílchez <dvilchez@xuaps.com>
2022-07-08 19:13:28 +0200

    Add some styles

--- src/ui/Layout.tsx
+++ src/ui/Layout.tsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import { Container } from "react-bootstrap";
 
 const Layout = ({ children }: { children: JSX.Element[] }) => {
-  return <Container>{children}</Container>;
+  return <Container style={{ marginTop: 20 }}>{children}</Container>;
 };
 
 Layout.propTypes = {
--- src/ui/Uploader.tsx
+++ src/ui/Uploader.tsx
@@ -1,13 +1,57 @@
-import React, { useEffect } from "react";
-import { Card, Placeholder } from "react-bootstrap";
+import React, { CSSProperties, useEffect, useMemo } from "react";
 import { useDropzone } from "react-dropzone";
 
+const baseStyle: CSSProperties = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
+};
+
+const focusedStyle = {
+  borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
+
 const Uploader = ({ onFileAdded }: { onFileAdded: (file: File) => void }) => {
-  const { acceptedFiles, getRootProps, getInputProps, isDragActive } =
-    useDropzone({
-      maxFiles: 1,
-      multiple: false,
-    });
+  const {
+    acceptedFiles,
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isFocused,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    maxFiles: 1,
+    multiple: false,
+  });
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isFocused ? focusedStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isFocused, isDragAccept, isDragReject]
+  );
 
   useEffect(() => {
     if (acceptedFiles.length === 1) {
@@ -16,24 +60,18 @@ const Uploader = ({ onFileAdded }: { onFileAdded: (file: File) => void }) => {
   }, [acceptedFiles]);
 
   return (
-    <>
+    <div className="container">
       <input data-testid="file-selector" {...getInputProps()} />
-      <Card {...getRootProps()}>
-        <Card.Body>
-          <Card.Title>Encrypt a file</Card.Title>
-          <Card.Text>
-            {isDragActive ? (
-              <p>Drop the files here ...</p>
-            ) : (
-              <p>
-                Drag &apos;n&apos; drop some files here, or click to select
-                files
-              </p>
-            )}
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </>
+      <div {...getRootProps({ style })}>
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>
+            Drag &apos;n&apos; drop some files here, or click to select files
+          </p>
+        )}
+      </div>
+    </div>
   );
 };
 
