627c43995d2ef8ee7f6f4f6645dc93237bbd9c53
David Vílchez <dvilchez@xuaps.com>
2022-10-12 14:11:08 +0200

    Remove honeycomb exporter

--- src/tracing.js
+++ src/tracing.js
@@ -1,19 +1,13 @@
 import {
-  ConsoleSpanExporter,
   SimpleSpanProcessor,
+  ConsoleSpanExporter,
 } from "@opentelemetry/sdk-trace-base";
 import { WebTracerProvider } from "@opentelemetry/sdk-trace-web";
 import { ZoneContextManager } from "@opentelemetry/context-zone";
-import { HoneycombExporter } from "opentelemetry-exporter-honeycomb";
 import { trace, context, SpanKind } from "@opentelemetry/api";
 
 const tracer = trace.getTracer("scytale", "0.1.0");
-const options = {
-  dataset: process.env.HONEYCOMB_DATASET,
-  writeKey: process.env.HONEYCOMB_WRITE_KEY,
-  serviceName: process.env.HONEYCOMB_SERVICE_NAME,
-};
-const exporter = new ConsoleSpanExporter(); //new HoneycombExporter(options);
+const exporter = new ConsoleSpanExporter();
 const provider = new WebTracerProvider();
 provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
 
