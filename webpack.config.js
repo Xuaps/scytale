16d6e7d4c499caefbd8bb54d78d35d61c37e7429
David Vílchez <dvilchez@xuaps.com>
2023-02-13 23:30:32 +0100

    Add manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
@@ -56,6 +57,53 @@ module.exports = ({ env }) => ({
       output: {
         filename: "index.html",
       },
+    }),
+    new WebpackPwaManifest({
+      name: "Scytale",
+      short_name: "Scytale",
+      start_url: ".",
+      display: "standalone",
+      background_color: "#fff",
+      theme_color: "#4285f4",
+      description: "Encrypt and decrypt files in a secure way",
+      crossorigin: null,
+      inject: true,
+      fingerprints: true,
+      ios: false,
+      publicPath: "./",
+      includeDirectory: true,
+      icons: [
+        {
+          src: path.resolve("public/assets/homescreen48.png"),
+          sizes: "48x48",
+          type: "image/png",
+        },
+        {
+          src: path.resolve("public/assets/homescreen72.png"),
+          sizes: "72x72",
+          type: "image/png",
+        },
+        {
+          src: path.resolve("public/assets/homescreen96.png"),
+          sizes: "96x96",
+          type: "image/png",
+        },
+        {
+          src: path.resolve("public/assets/homescreen144.png"),
+          sizes: "144x144",
+          type: "image/png",
+        },
+        {
+          src: path.resolve("public/assets/homescreen168.png"),
+          sizes: "168x168",
+          type: "image/png",
+        },
+        {
+          src: path.resolve("public/assets/homescreen192.png"),
+          sizes: "192x192",
+          type: "image/png",
+        },
+      ],
     }),
     new WorkboxPlugin.InjectManifest({
       maximumFileSizeToCacheInBytes: 4194304,
