aba37c4d0ac4be0c2f2a67bb81782c4764163f08
David Vílchez <dvilchez@xuaps.com>
2023-02-13 18:40:50 +0100

    Fix service worker route

--- src/Main.tsx
+++ src/Main.tsx
@@ -7,7 +7,7 @@ import { App } from "./ui";
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("sw.js")
       .then((registration) => {
         console.log("SW registered: ", registration);
       })
